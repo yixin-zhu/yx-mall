cf02a7dbebbbb67146bd11d4bd851698fbd0d0ce
Zhu Yixin <67551555+yixin-zhu@users.noreply.github.com>
2025-08-31 15:10:42 +0200

    ID generator

---
+++ src/main/java/com/hmdp/utils/RedisIdWorker.java
@@ -0,0 +1,44 @@
+package com.hmdp.utils;
+
+import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.stereotype.Component;
+
+import java.time.LocalDateTime;
+import java.time.ZoneOffset;
+import java.time.format.DateTimeFormatter;
+
+// 用于生成全局唯一id
+// id结构：0(1bit) + 时间戳(31bit) + 序列号(32bit)
+@Component
+public class RedisIdWorker {
+    /**
+     * 开始时间戳 2025-01-01 00:00:00
+     */
+    private static final long BEGIN_TIMESTAMP = 1735686000L;
+    /**
+     * 序列号的位数
+     */
+    private static final int COUNT_BITS = 32;
+
+    private StringRedisTemplate stringRedisTemplate;
+
+    public RedisIdWorker(StringRedisTemplate stringRedisTemplate) {
+        this.stringRedisTemplate = stringRedisTemplate;
+    }
+
+    public long nextId(String keyPrefix) {
+        // 1.生成时间戳
+        LocalDateTime now = LocalDateTime.now();
+        long nowSecond = now.toEpochSecond(ZoneOffset.UTC);
+        long timestamp = nowSecond - BEGIN_TIMESTAMP;
+
+        // 2.生成序列号
+        // 2.1.获取当前日期，精确到天
+        String date = now.format(DateTimeFormatter.ofPattern("yyyy:MM:dd"));
+        // 2.2.自增长
+        long count = stringRedisTemplate.opsForValue().increment("icr:" + keyPrefix + ":" + date);
+
+        // 3.拼接并返回
+        return timestamp << COUNT_BITS | count;
+    }
+}
